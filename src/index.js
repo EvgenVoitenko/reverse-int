a81eebbc9e2c41ebc715614f792812856c04def6
EvgenVoitenko <evgenvoitenko@gmail.com>
2022-07-06 15:37:24 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  11
+let result = '';
+let nString = Math.abs(n).toString();
+let i = (nString.length - 1);
+while (i >= 0) {
+    result = `${result}${nString[i]}`;
+    i = i - 1;
+}
+return +result;
 }
